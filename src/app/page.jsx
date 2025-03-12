ec8455e0df284f2e9dfafc37e34af12871b1c15e
arufaki <fakihkazorla@gmail.com>
2025-03-13 00:46:54 +0700

    feat(header): add header component with icons

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Bell, House, MessageCircle, Search, Settings, Users } from "lucide-react";
 
 const Page = () => {
-    return <div>Page</div>;
+    return (
+        <header className="mx-5 my-4">
+            <div className="flex flex-row items-center justify-between">
+                <h1 className="font-bold text-2xl text-gray-700">Zynk</h1>
+                <div className="flex flex-row gap-2">
+                    <Search className="text-gray-700" />
+                    <Settings className="text-gray-700" />
+                </div>
+            </div>
+            <div className="flex flex-row justify-evenly my-2">
+                <House className="text-gray-700" size={25} />
+                <Users className="text-gray-700" size={25} />
+                <MessageCircle className="text-gray-700" size={25} />
+                <Bell className="text-gray-700" size={25} />
+            </div>
+        </header>
+    );
 };
 
 export default Page;
