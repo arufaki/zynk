3fc11d91b4ad40a33b566f168f792a3e6334569f
arufaki <fakihkazorla@gmail.com>
2025-03-17 08:09:39 +0700

    feat(ui): add post list component to page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,25 @@
+import CardPost from "./_components/Card/CardPost";
+import PostList from "./_components/Card/PostList";
 import Modal from "./_components/Modal";
 import ModalTrigger from "./_components/ModalTrigger";
 
 const Page = () => {
     return (
-        <section className="mx-5 mb-4 mt-6">
-            <ModalTrigger modalId="my_modal_3" className="w-full px-3 py-2 border border-gray-300 text-sm text-gray-600 rounded">
-                Make a post...
-            </ModalTrigger>
-            <Modal id="my_modal_3">
-                <h3 className="font-bold text-lg mb-3 text-gray-700">Buat Postingan!</h3>
-                <textarea className="textarea h-24 w-full focus:outline-none focus:ring-0 mb-3" placeholder="Apa yang anda pikirkan, Fura?"></textarea>
-                <button className="btn btn-outline w-full border border-gray-300 text-gray-700">Kirim!</button>
-            </Modal>
-        </section>
+        <>
+            <section className="mx-5 mb-4 mt-6">
+                <ModalTrigger modalId="my_modal_3" className="w-full px-3 py-2 border border-gray-300 text-sm text-gray-600 rounded">
+                    Make a post...
+                </ModalTrigger>
+                <Modal id="my_modal_3">
+                    <h3 className="font-bold text-lg mb-3 text-gray-700">Buat Postingan!</h3>
+                    <textarea className="textarea h-24 w-full focus:outline-none focus:ring-0 mb-3" placeholder="Apa yang anda pikirkan, Fura?"></textarea>
+                    <button className="btn btn-outline w-full border border-gray-300 text-gray-700">Kirim!</button>
+                </Modal>
+            </section>
+            <section className="mx-5 mb-4 mt-6">
+                <PostList />
+            </section>
+        </>
     );
 };
 
