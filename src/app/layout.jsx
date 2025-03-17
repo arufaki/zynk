3e96e9942c633c7da90a0db6b6b1afd7b9024ddf
arufaki <fakihkazorla@gmail.com>
2025-03-17 08:09:29 +0700

    refactor(ui): remove navbar component from layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,6 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
-import Navbar from "./_components/Navbar";
 
 const roboto = Roboto({
     subsets: ["latin"],
@@ -12,11 +11,8 @@ const RootLayout = ({ children }) => {
     return (
         <html lang="en" className={roboto.className} data-theme="light">
             <body cz-shortcut-listen="true">
-                <main>
-                    <Header />
-                    <Navbar />
-                    {children}
-                </main>
+                <Header />
+                <main>{children}</main>
             </body>
         </html>
     );
