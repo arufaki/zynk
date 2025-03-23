c59acf3b89e17c8d02b35fa23cd4563907627dd4
arufaki <fakihkazorla@gmail.com>
2025-03-24 00:06:39 +0700

    feat(auth): add login page component

---
+++ src/app/(authentication)/login/page.jsx
@@ -0,0 +1,10 @@
+const Page = () => {
+    return (
+        <div className="text-center pt-8 pb-2">
+            <h1 className="text-gray-700 font-bold text-2xl">Masuk</h1>
+            <h2 className="text-gray-400 text-base my-5">Silahkan masuk ke akun anda</h2>
+        </div>
+    );
+};
+
+export default Page;
